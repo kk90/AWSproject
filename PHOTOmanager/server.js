923bb5352090a94b17bc1200ff575705eeb09634
kk90 <kr.korycki@gmail.com>
2014-06-19 18:51:26 +0200

    view

--- PHOTOmanager/server.js
+++ PHOTOmanager/server.js
@@ -7,15 +7,28 @@ app.set('view engine', 'ejs');
 app.use(express.static("views/kickstartJS"));
 
 var AWS = require("aws-sdk");
-// Configuration 
+
 
 
 
 // Routes 
 app.get( '/', function ( req, res ) {
     
-    res.render('index', { title:"Lista folderów" });
+    res.render('galery', { title:"Galeria" });
    
 });
+
+app.get( '/upload', function ( req, res ) {
+    
+    res.render('upload', { title:"Wczytaj" });
+   
+});
+
+app.get( '/convert', function ( req, res ) {
+    
+    res.render('convert', { title:"Konwertuj" });
+   
+});
+
 
 app.listen( 8080 ); 
