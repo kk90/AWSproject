9b91cffe471a1de600db92de0a4f5b7b178f1c49
kk90 <kr.korycki@gmail.com>
2014-06-19 17:39:24 +0200

    initial commit

---
+++ PHOTOmanager/server.js
@@ -0,0 +1,18 @@
+﻿
+var express = require( 'express' );
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static("views/kickstartJS"));
+var AWS = require("aws-sdk");
+// Configuration 
+
+
+
+// Routes 
+app.get( '/', function ( req, res ) {
+    
+    res.render('layout', { list: list,title:"Lista folderów" });
+   
+});
+
+app.listen( 8080 ); 
