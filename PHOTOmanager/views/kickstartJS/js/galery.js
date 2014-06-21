430a29f9ac19cbccd6f211fc5576ea2d8042da79
kk90 <kr.korycki@gmail.com>
2014-06-21 19:48:27 +0200

    added context menu, dynamic loading,

--- PHOTOmanager/views/kickstartJS/js/galery.js
+++ PHOTOmanager/views/kickstartJS/js/galery.js
@@ -1,61 +1,102 @@
-﻿function loadImages() {
+﻿
+
+function pushItem( pictureObject ) {
+
+    var mainIMG = $( '<img>' ).attr( {
+        src: pictureObject.uri,
+        class: "imageItem",
+        width: 100,
+        height: 100,
+        imageKey: pictureObject.key
+    });
+
+    var mainA = $( '<a>', {
+        href: pictureObject.uri
+    })
+
+               mainA.append( mainIMG );
+
+    var menu1 = [
+        { 'Usuń': function ( menuItem, menu ) { if ( confirm( 'Usunąć plik?' ) ) {
+      //$("#gallery").empty();
+      //$("#waitScreen").show();
+      $( this ).parent().hide();
+      $.ajax( {
+          url: "deleteImage?key=" + $( this ).attr( "imagekey" ),
+          context: document.body
+      }).done( function ( data ) {
+
+          });
+
+  } } },
+        $.contextMenu.separator,
+        { 'Edytuj': function ( menuItem, menu ) { alert( "You clicked Option 2!" ); } }
+    ];
+    
+         mainIMG.contextMenu(menu1);
+    
+
+    /* mainIMG.mousedown( function ( e ) {
+if ( e.button == 2 ) {
+
+  if ( confirm( 'Usunąć plik?' ) ) {
+      //$("#gallery").empty();
+      //$("#waitScreen").show();
+      $( this ).parent().hide();
+      $.ajax( {
+          url: "deleteImage?key=" + $( this ).attr( "imagekey" ),
+          context: document.body
+      }).done( function ( data ) {
+
+          });
+
+  } else {
+
+  }
+
+  return false;
+}
+return true;
+});*/
+
+    $( "#gallery" ).append( mainA );
+
+
+
+
+}
+
+
+function loadImages() {
+
     $.ajax( {
         url: "getImages",
         context: document.body
-    }).done( function (data) {
-        console.log(data);
-           $("#waitScreen").hide();
-           for(var i=0;i<data.length ;i++){
-               
-               var mainIMG = $('<img>').attr({
-			    src : data[i].uri,
-                class:"imageItem",
-                width:100,
-                height:100,
-                imageKey:data[i].key
-		        });
-
-               var mainA = $('<a>', {
-			        href:data[i].uri
-		        })
-
-               mainA.append(mainIMG);
-              
-
-               $("#gallery").append(mainA);
-               }
-
-               $(".imageItem").mousedown(function(e){ 
-    if( e.button == 2 ) { 
-        
-        if (confirm('Usunąć plik?')) {
-            $("#gallery").empty();
-            $("#waitScreen").show();
-            $.ajax( {
-            url: "deleteImage?key="+$(this).attr("imagekey"),
-            context: document.body
-            }).done( function (data) {
-                loadImages();
-        });
-
-        } else {
-    
-        }
-
-      return false; 
-    } 
-    return true; 
-  }); 
-
+    }).done( function ( data ) {
+            imageObjects = data;
+            $( "#waitScreen" ).hide();
+            for ( var i = 0; i < data.length; i++ ) {
+                pushItem( data[i] );
+            }
         });
 
 
 };
 
 
-$( document ).ready(function() {
-  loadImages();
+$( document ).ready( function () {
+    loadImages();
+
+
 });
+
+
+
+function refresh() {
+    $( "#waitScreen" ).show();
+    $( "#gallery" ).empty();
+    loadImages();
+}
 
 /*    <% for(var i=0; i<10; i++){ %>
  <a href="http://placehold.it/1x450/4D99E0/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/4D99E0/ffffff.png&text=100x100" width="100" height="100" /></a>
