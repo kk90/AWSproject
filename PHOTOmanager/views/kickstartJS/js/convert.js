98c5b56ef4549d184725d999b84637b17454a2c2
kk90 <kr.korycki@gmail.com>
2014-06-25 22:45:22 +0200

    travis config

---
+++ PHOTOmanager/views/kickstartJS/js/convert.js
@@ -0,0 +1,26 @@
+﻿
+function parseParameters() {
+
+    var conversionquery = "";
+    if ($('#check1')[0].checked) {
+        conversionquery+="negatyw|"
+    }
+
+    if ($('#check2')[0].checked) {
+        conversionquery += "sepia|"
+    }
+    if ($('#check3')[0].checked) {
+        conversionquery += "fantazja|"
+    }
+    return conversionquery;
+}
+
+
+function convertFile(id){
+    $.ajax( {
+        url: "pushConvert?key=" + id + "&conversion=" + parseParameters(),
+          context: document.body
+      }).done( function ( data ) {
+          $('#mainImage').attr("src","/img/processing.gif")
+          });
+}
